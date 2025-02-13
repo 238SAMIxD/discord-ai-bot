b146aded14bd65652eca4601e1b0fe723d994c9d
Samuel Jędrzejewski <samijedrzejewski@gmail.com>
2025-02-13 16:14:21 +0100

    Added channels command

---
+++ src/commands/channels.ts
@@ -0,0 +1,30 @@
+import { CommandInteraction, EmbedBuilder, SlashCommandBuilder } from "discord.js";
+
+import { CHANNELS } from "../bot";
+import commands from "./commands";
+
+async function channels() {
+  return {
+    command: new SlashCommandBuilder()
+      .setName("channels")
+      .setDescription("Check channels in which the bot is active"),
+    handler: handleChannels,
+  };
+
+  async function handleChannels(interaction: CommandInteraction) {
+    if (CHANNELS.length === 0) {
+      await interaction.reply({
+        content: "Bot is available in all channels.",
+        ephemeral: true,
+      });
+      return;
+    }
+
+    await interaction.reply({
+      content: `Bot is available in the following channels: ${CHANNELS.map(channel => `<#${channel}>`).join(", ")}`,
+      ephemeral: true,
+    });
+  }
+}
+
+export default channels;
--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -1,9 +1,10 @@
+import channels from "./channels";
 import chat, { ChatOptions } from "./chat";
 import help from "./help";
 import ping from "./ping";
 import systemMessage from "./systemMessage";
 import text2img, { Text2ImgOptions } from "./text2img";
 
-export default [chat, text2img, ping, systemMessage, help];
+export default [chat, text2img, ping, systemMessage, help, channels];
 
 export type CommandsOptions = Text2ImgOptions | ChatOptions;
